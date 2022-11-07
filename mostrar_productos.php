69249310d68dcf86baec61c64fbd11b71ce0934b
leankidd <74878178+leankidd@users.noreply.github.com>
2022-11-07 10:13:39 -0300

    .

--- mostrar_productos.php
+++ mostrar_productos.php
@@ -11,7 +11,7 @@ $con =  $conexion -> query($contar);
 $visado = $con -> fetch_array();
 ?>
 <table border = 1>
-<tr><td>Nombre</td><td>Marca</td><td>Precio</td><td>Fecha del precio</td><td>Moneda</td><td>IVA</td><td>Imagen</td><td>Caracteristica 1</td><td>Caracteristica 2</td><td>Caracteristica 3</td><td>Caracteristica 4</td><td>Caracteristica 5</td><td>Caracteristica 6</td>
+<tr><td>Nombre</td><td>Marca</td><td>Valor</td><td>Fecha del valor</td><td>Moneda</td><td>IVA</td><td>Caracteristica 1</td><td>Carac. 2</td><td>Carac. 3</td><td>Carac. 4</td><td>Carac. 5</td><td>Carac. 6</td><td>Imagen</td>
 <?php
 $ID = 0;
 for($i = 0; $i < $visado[0]; $i ++)
@@ -24,6 +24,14 @@ for($i = 0; $i < $visado[0]; $i ++)
     $con2 =  $conexion -> query($contar2);
     $visado2 = $con2 -> fetch_array();
     $ID_carac = 0;
+    if($ionar[11] == "Pesos")
+    {
+        $valor = "$";
+    }
+    else
+    {
+        $valor = "U$"."S";
+    }
     for($x = 0; $x < $visado2[0]; $x ++)
     {
         $sel2 = "SELECT * from carac_prod where ID_prod = $ionar[0] and ID_carac_prod > $ID_carac order by ID_carac_prod asc";
@@ -39,8 +47,10 @@ for($i = 0; $i < $visado[0]; $i ++)
             $carac[$x] = '-';
         }
     }
+    $imagen = $ionar[7];
+    $img="imagenes_subidas/".$imagen;
     ?>
-    <tr><td><?php echo $ionar[2]?></td><td><?php echo $ionar[3]?></td><td><?php echo $ionar[8]?></td><td><?php echo $ionar[10]?></td><td><?php echo $ionar[11]?></td><td><?php echo $ionar[12]?></td><td><?php echo "hoola"?></td><td><?php echo $carac[0]?></td><td><?php echo $carac[1]?></td><td><?php echo $carac[2]?></td><td><?php echo $carac[3]?></td><td><?php echo $carac[4]?></td><td><?php echo $carac[5]?></td></tr>
+    <tr><td><?php echo $ionar[2]?></td><td><?php echo $ionar[3]?></td><td><?php echo $valor.$ionar[8]?></td><td><?php echo $ionar[10]?></td><td><?php echo $ionar[11]?></td><td><?php echo $ionar[12]?></td><td><?php echo $carac[0]?></td><td><?php echo $carac[1]?></td><td><?php echo $carac[2]?></td><td><?php echo $carac[3]?></td><td><?php echo $carac[4]?></td><td><?php echo $carac[5]?></td><td><?php echo '<img src= "'.$img.'">'?></td></tr>
 
 
     <?php
